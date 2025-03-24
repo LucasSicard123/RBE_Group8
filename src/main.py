e09a9180caadee561336cd24f2cf59328286060b
Arrowknee <dch3902@gmail.com>
2025-03-24 09:21:37 -0400

    Removed finished lab section, added in logic(duplicate) of functions for Lab 1.B

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
+
+
+        
 # ---------------------------------------------------------------------------- #
 #                                                                              #
-# 	Module:       main.py                                                      #
-# 	Author:       Lucas                                                        #
-# 	Created:      3/19/2025, 8:32:54 AM                                        #
-# 	Description:  V5 project                                                   #
+# Module:       main.py                                                      #
+# Author:       Charles Doh                                                      #
+# Created:      3/21/2025, 8:32:54 AM                                        #
+# Description:  V5 project                                                   #
 #                                                                              #
 # ---------------------------------------------------------------------------- #
 
@@ -14,27 +17,35 @@ import time
 # Brain should be defined by default
 brain = Brain()
 
-brain.screen.print("Hello V5")
+wheelTrack = 11 #in inches
+wheelDiameter = 4 #in inches
+gearRatio = 5 #5 : 1 // 60 : 12
+wheelCircumference = 3.14 * wheelDiameter
+degreesPerInch = 360.0 / wheelCircumference
+
+#Instantiations
+brain.screen.print("Hello V5, Team 8")
 
 left_motor = Motor(Ports.PORT2, 18_1, True)
 right_motor = Motor(Ports.PORT1, 18_1, False)
 
-# Move left wheels 1 full rotation
-left_motor.spin(FORWARD, 30, RPM)
-time.sleep(10)
-left_motor.stop(BRAKE)
+def turnDegrees(n_degrees): 
+    degreesToRotatePerRobotRotation = (wheelTrack)/(wheelDiameter) #Degrees to turn per robot rotational degree.
+    toTurn = degreesToRotatePerRobotRotation * n_degrees
+    left_motor.spin_for(REVERSE, toTurn, DEGREES, 30, RPM, False)
+    right_motor.spin_for(FORWARD, toTurn, DEGREES, 30, RPM, True)
 
-# Move right wheels 1 full rotation
-right_motor.spin(FORWARD, 30, RPM)
-time.sleep(10)
-right_motor.stop(BRAKE)
+def moveInches(n_inches): 
+    degreesToTravel = n_inches * (degreesPerInch) # 1 : 1
+    degreesToRotate = degreesToTravel * 5 #5 : 1, now it is gear ratio compliant
+    left_motor.spin_for(FORWARD, degreesToRotate, DEGREES, 100, RPM, False)
+    right_motor.spin_for(FORWARD, degreesToRotate, DEGREES, 100, RPM, True)
 
-# Move both wheels 2 full rotations
-left_motor.spin(FORWARD, 30, RPM)
-right_motor.spin(FORWARD, 30, RPM)
-time.sleep(20)
-left_motor.stop(BRAKE)
-right_motor.stop(BRAKE)
+def polygon(n_sides, n_length):
+    degreesPerTurn = 360/n_sides
+    for i in n_sides : 
+        moveInches(n_length)
+        turnDegrees(360/n_sides)
 
-# Done!
-brain.screen.print("How did I do?")
+#Function calls begin again here
+polygon(4,5)
