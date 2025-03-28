f8f03cc33b263da223e57643f740ba0bd3455e29
Arrowknee <dch3902@gmail.com>
2025-03-28 11:18:51 -0400

    Wild card

--- src/drivetrainController.py
+++ src/drivetrainController.py
@@ -8,13 +8,20 @@ class drivetrainController:
         self.brain = brain
         self.left_motor = left_motor
         self.right_motor = right_motor
+    def clamp(self, value, min_value, max_value):
+        return max(min_value, min(value, max_value))
+    def movePercent(self, n_percent, n_percentDirection): 
+        self.left_motor.spin(FORWARD, self.clamp(n_percent - n_percentDirection, -1, 1), PERCENT)
+        self.right_motor.spin(FORWARD, self.clamp(n_percent + n_percentDirection, -1, 1), PERCENT)
+        return
     def drive(self): 
         toDrive = True
         while toDrive: 
             #Normalized values [-1,1]
             drive_r = self.VEXcontroller.axis3.position #rotational 
             drive_f = self.VEXcontroller.axis2.position #speed control
-    def movePercent(self, n_percent, speed_rpm): 
-        self.left_motor.spin(FORWARD, n_percent, PERCENT)
-        self.right_motor.spin(FORWARD, n_percent, PERCENT)
-        return
+
+            self.movePercent(drive_f, drive_r)
+            if self.VEXcontroller.buttonA.pressing :
+                self.movePercent(0,0) 
+                toDrive = False
--- src/main.py
+++ src/main.py
@@ -28,7 +28,10 @@ left_motor = Motor(Ports.PORT2, 18_1, True)
 right_motor = Motor(Ports.PORT1, 18_1, False)
 rangeFinderFront = Sonar(brain.three_wire_port.g)
 rangeFinderSide = Sonar(brain.three_wire_port.a)
-#DriveTraincontroller = drivetrainController(controller, brain, left_motor, right_motor) #experimental as hell 
+# gyro = Gyro(brain.three_wire_port.c) #For later (Gyroscope)
+# inertial = Inertial(brain.three_wire_port.d) #For later (Inertial Sensor)
+
+# DriveTraincontroller = drivetrainController(controller, brain, left_motor, right_motor) #experimental as hell 
 
 #!Turns Robot among rotational center n degrees COUNTER CLOCKWISE
 def turnDegrees(n_degrees): 
@@ -49,10 +52,15 @@ def movePercent(n_percent, speed_rpm):
     left_motor.spin(FORWARD, n_percent, PERCENT)
     right_motor.spin(FORWARD, n_percent, PERCENT)
 
+def stopMotors():
+    left_motor.stop(BrakeType.BRAKE)
+    right_motor.stop(BrakeType.BRAKE)
+
 #Helper function, this will be used often to validate input
 def clamp(value, min_value, max_value):
     return max(min_value, min(value, max_value))
 
+#! Inclusive bounds. 
 def rangeCheck(value, min_Value, max_value): 
     if value >= min_Value and value <= max_value:
         return True
@@ -65,35 +73,87 @@ def drive(speed_rpm, n_direction):
     right_motor.set_velocity(speed_rpm + (n_direction), RPM)
     left_motor.spin(FORWARD)
     right_motor.spin(FORWARD)
-    # sleep(10) #10 millisecond cooldown
-    # if controller.buttonA.pressed: 
-    #     left_motor.stop(BrakeType.BRAKE)
-    #     right_motor.stop(BrakeType.BRAKE)
     return
 
 #lab.2 function 
 def wallFollowInches(setDistanceFromwall): 
-    kP = 10
+    kP = 2
     kI = 0
     kD = 0
     FeedForward = 0
+    tolerance = 0.2
     distanceFromWall_side = rangeFinderSide.distance(DistanceUnits.IN) #Inches
     distanceFromWall_front = rangeFinderFront.distance(DistanceUnits.IN) #Inches
-    error_front = distanceFromWall_front - 8
-    error_directional =  distanceFromWall_side - setDistanceFromwall #Actual - setpoint = negative(goes backward) when bot is too close, otherwise it goes towards the wall
-
-    if distanceFromWall_front > 8 : 
-        drive(75,-kP * error_directional) #this looks sus to me
-    else : 
-        drive(0,0) #stop first
-        turnDegrees(90) # command based completion
+    # error_front = distanceFromWall_front - 8 #Made obsolete(for now)
+    error_directional =   setDistanceFromwall - distanceFromWall_side #Actual - setpoint = negative(goes backward) when bot is too close, otherwise it goes towards the wall
+    notComplete = True
+    #Code for part 1: Dead reckoning (Requires a lot of 'if' conditions, and it pisses me off)
+    turnCount = 0
     
 
-#Function calls begin again here
+
+    #Code for part 2: Inertial sensors
+
+    #Use #turnwithGyroInPlace(90)
+
+
+    #Dead-reckoning version as of now
+    while notComplete:
+        if not(rangeCheck(distanceFromWall_front, 8-tolerance, 8+tolerance)) and turnCount < 1: 
+            error_directional =  distanceFromWall_side - setDistanceFromwall #update
+            error_directional = clamp(error_directional, -11, 50) #Set a limit on how far the wall can even be, otherwise we deal with ridiculous turn values
+
+            # drive(75, kP * error_directional) #this looks sus to me
+            brain.screen.print(error_directional)
+            print(str(error_directional) + "\n")
+            wait(100)
+            brain.screen.set_cursor(1,1)
+            brain.screen.clear_screen()
+        else : 
+            if turnCount == 1 and not(rangeCheck(distanceFromWall_front, 0 - tolerance, 0+tolerance)): #TODO: add proper values, measure distance from center point of the two trees to the closest front wall
+                error_directional =  distanceFromWall_side - setDistanceFromwall #update
+                error_directional = clamp(error_directional, -11, 50) #Same clamp as earlier
+                drive(75, kP * error_directional)
+                wait(10) 
+            else :
+                drive(0,0) #stop first
+                wait(10)
+                turnDegrees(90) # command based completion
+                turnCount += 1
+                if turnCount >= 2: 
+                    stopMotors()
+                    notComplete = False
+
+#! should be used with precision, setPoint is in Degrees(radians requires too many PI calls, and I am too lazy)
+# def turnWithGyroInPlace(setPoint):
+#     setPoint %= 360
+#     reachedSetpoint = False
+
+#     kP = 1/360 #To be determined more accurately, this is a guess
+#     kI = 0
+#     kD = 0
+#     error = setPoint - inertial.orientation(OrientationType.YAW)
+
+#     tolerance = 3
+#     while not reachedSetpoint: 
+#         left_motor.set_velocity((kP * error),RPM )
+#         right_motor.set_velocity((kP * error), RPM)
+#         left_motor.spin(FORWARD)
+#         right_motor.spin(FORWARD)
+#         reachedSetpoint = rangeCheck(error, -3, 3)
+
+#     stopMotors()
+#     return 
+
+################################################################################
+#Function calls begin here
 # polygon(6,5)
 # solveMaze()
+controller.buttonA.pressed(stopMotors) #Predefine a easy to press E-stop just in case. 
 wait(2000) # Wait time for the Sonar to catch up, and actually gives read values
 wallFollowInches(11) # 11 Inches from the wall
+
+
 # brain.screen.set_cursor(1,1)
 # while True : 
 #     brain.screen.print(rangeFinderSide.distance(DistanceUnits.IN))
